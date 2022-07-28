b4025b0dd013889c0eaa279dc3c4f8418ab7125d
David Martin <martin.david1995@yahoo.com>
2022-07-28 12:18:53 -0400

    initial auth setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import Counter from './components/Counter'
 import logo from './logo.svg'
 import './App.css'
-// import Login from './components/login/Login'
+import Login from './components/login/Login'
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
+			{/* <header className="App-header">
 				<Counter />
-			</header>
-			{/* <Login /> */}
+			</header> */}
+			<Login />
 		</div>
 	)
 }
