0407fcec593edff853c0f5e1ebbf3725cfc53eb2
David Martin <martin.david1995@yahoo.com>
2022-07-29 20:49:34 -0400

    rework authentication system for email/password

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendPasswordResetEmail } from 'firebase/auth'
+
+const AuthContext = React.createContext()
+const auth2 = getAuth()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider( {children} ) {
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+  
+  function signup(email, password){
+    return createUserWithEmailAndPassword(auth2, email, password)
+  }
+
+  function login(email, password){
+    return signInWithEmailAndPassword(auth2, email, password)
+  }
+
+  function logout() {
+    return signOut(auth2)
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth2, email)
+  }
+
+  function updateEmail(email) {
+    return updateEmail(getAuth().currentUser, email)
+  }
+
+  function updatePassword(password) {
+    return updatePassword(getAuth.currentUser, password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+        setCurrentUser(user)
+        setLoading(false)
+      })
+
+    return unsubscribe
+  }, [])
+
+  
+
+  const value = {
+    currentUser,
+    login,
+    logout,
+    signup,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+        {!loading && children}
+    </AuthContext.Provider>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import './index.css'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 )
